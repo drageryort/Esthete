1ae32da7c6bbc9b5307bc7d2e152706121282676
Anton Vorobiov <drageryort@gmail.com>
2023-01-19 00:55:15 +0200

    1203765888730886

--- src/components/common-blocks/gallery/AppGallery.vue
+++ src/components/common-blocks/gallery/AppGallery.vue
@@ -4,25 +4,26 @@
       <h2 class="title wow-total animate__fadeInUp" v-html="pageData['galleryTitle']"></h2>
       <div class="gallery">
         <AppGalleryEl v-for="(el, index) in pageData['galleryList']"
-                      :key="el['galleryListProductName']"
-                      :elementData="el"
-                      @click="galleryModal(true, index, el['galleryListProductHash'])"
+                      :key="el['ID']"
+                      :elementData="el['acf']"
+                      @click="galleryModal(true, index, el['acf']['galleryListProductHash'])"
         />
       </div>
       <div class="show-more-cases-wrapper wow-total animate__fadeInUp">
         <router-link to="/portfolio" class="btn show-more-cases">
           <span class="text">{{ pageData['galleryButtonText'] }}</span>
-          <img class="circled-blue-arrow" src="@/assets/images/svg/arrow-right-circle-blue-icon.svg" alt="show more icon">
+          <img class="circled-blue-arrow" src="@/assets/images/svg/arrow-right-circle-blue-icon.svg"
+               alt="show more icon">
         </router-link>
       </div>
     </div>
   </div>
   <transition name="slide">
     <AppGalleryModal
-      v-if="galleryModalActive"
-      @galleryModal="galleryModal"
-      :modalData="pageData['galleryList'][currentIndex]"
-      :pageData="pageData"
+        v-if="galleryModalActive"
+        @galleryModal="galleryModal"
+        :modalData="pageData['galleryList'][currentIndex]['acf']"
+        :pageData="pageData"
     />
   </transition>
 </template>
@@ -39,7 +40,7 @@ export default defineComponent({
   },
   components: {AppGalleryEl, AppGalleryModal},
   data() {
-    return{
+    return {
       galleryModalActive: false,
       currentIndex: 0,
       modalChecked: false
@@ -55,38 +56,35 @@ export default defineComponent({
     routeName() {
       return this.$route['name'];
     },
-    galleryList(){
-      if(typeof this.pageData === "object"){
+    galleryList() {
+      if (typeof this.pageData === "object") {
         return this.pageData['galleryList']
       }
-      else{
-        return []
-      }
+      return []
     }
   },
   methods: {
-    galleryModal(trigger:boolean, cardIndex:number, hashName:string) {
+    galleryModal(trigger: boolean, cardIndex: number, hashName: string) {
       this.galleryModalActive = trigger;
       this.currentIndex = cardIndex;
       trigger ? document.body.style.overflowY = "hidden" : document.body.style.overflowY = "auto"
-      if(trigger && !window.location.hash && this.routeName !== "home"){
+      if (trigger && !window.location.hash && this.routeName !== "home") {
         location.hash = hashName
-      } else if(!trigger){
-        history.replaceState(undefined,'',this.routePath)
+      } else if (!trigger) {
+        history.replaceState(undefined, '', this.routePath)
       }
     },
-    galleryModalHash(galleryData:number[]){
-      if(
-          typeof galleryData === "object"  &&
-          galleryData.length &&
+    galleryModalHash(galleryData: number[]) {
+      if (
           !this.modalChecked &&
           this.routeName != "home" &&
           this.routeHash
-      ){
-        let index = galleryData.findIndex((elem:object | any) => {
-          return elem['galleryListProductHash'] ===  this.routeHash
+      ) {
+        let index = galleryData.findIndex((elem: object | any) => {
+          return elem['acf']['galleryListProductHash'] === this.routeHash
         })
-        if(index>=0){
+        console.log(index);
+        if (index >= 0) {
           this.galleryModal(true, index, '')
           this.modalChecked = true
         }
@@ -96,8 +94,8 @@ export default defineComponent({
   mounted() {
     this.galleryModalHash(this.galleryList)
   },
-  watch:{
-    galleryList(newData, oldData){
+  watch: {
+    galleryList(newData) {
       this.galleryModalHash(newData)
     }
   }
@@ -105,86 +103,105 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
-  .gallery-block{
-    padding: 60px 0 80px;
-    .title{
-      width: 525px;
-      font-weight: 300;
-      font-size: 30px;
+.gallery-block {
+  padding: 60px 0 80px;
+
+  .title {
+    width: 525px;
+    font-weight: 300;
+    font-size: 30px;
+    line-height: 120%;
+  }
+
+  .gallery {
+    display: grid;
+    grid-template-columns: 525px 525px;
+    gap: 60px;
+    margin: 80px 0 0;
+  }
+
+  .btn {
+    display: flex;
+    align-items: center;
+    column-gap: 10px;
+    margin: 80px 0 0;
+
+    .text {
+      font-weight: 500;
+      font-size: 20px;
+      line-height: 20px;
+    }
+
+    .circled-blue-arrow {
+      width: 30px;
+      height: 30px;
+    }
+  }
+
+}
+
+@media (max-width: 1025px) {
+  .gallery-block {
+    padding: 20px 0 60px;
+
+    .title {
+      width: 510px;
+      font-size: 26px;
       line-height: 120%;
     }
-    .gallery{
+
+    .gallery {
       display: grid;
-      grid-template-columns: 525px 525px;
-      gap: 60px;
-      margin: 80px 0 0;
+      grid-template-columns: 326px 326px;
+      gap: 30px;
+      margin: 40px 0 0;
     }
-    .btn{
-      display: flex;
-      align-items: center;
-      column-gap: 10px;
-      margin: 80px 0 0;
-      .text{
+
+    .btn {
+      margin: 40px 0 0;
+
+      .text {
         font-weight: 500;
-        font-size: 20px;
+        font-size: 16px;
         line-height: 20px;
       }
-      .circled-blue-arrow{
-        width: 30px;
-        height: 30px;
+
+      .circled-blue-arrow {
+        width: 26px;
+        height: 26px;
       }
     }
-    
+
   }
-  @media (max-width: 1025px){
-    .gallery-block{
-      padding: 20px 0 60px;
-      .title{
-        width: 510px;
-        font-size: 26px;
-        line-height: 120%;
-      }
-      .gallery{
-        display: grid;
-        grid-template-columns: 326px 326px;
-        gap: 30px;
-        margin: 40px 0 0;
-      }
-      .btn{
-        margin: 40px 0 0;
-        .text{
-          font-weight: 500;
-          font-size: 16px;
-          line-height: 20px;
-        }
-        .circled-blue-arrow{
-          width: 26px;
-          height: 26px;
-        }
+}
+
+@media (max-width: 660px) {
+  .gallery-block {
+    padding: 20px 0 40px;
+
+    .title {
+      max-width: 100%;
+      font-size: 20px;
+      line-height: 120%;
+      text-align: center;
+    }
+
+    .gallery {
+      display: flex;
+      flex-direction: column;
+    }
+
+    .btn {
+      margin: 40px auto 0;
+      justify-content: center;
+
+      .text {
       }
 
+      .circled-blue-arrow {
+      }
     }
-  }
-  @media (max-width: 660px){
-    .gallery-block{
-      padding: 20px 0 40px;
-      .title{
-        max-width: 100%;
-        font-size: 20px;
-        line-height: 120%;
-        text-align: center;
-      }
-      .gallery{
-        display: flex;
-        flex-direction: column;
-      }
-      .btn{
-        margin: 40px auto 0;
-        justify-content: center;
-        .text{}
-        .circled-blue-arrow{}
-      }
 
-    }
   }
+}
 </style>
--- src/components/portfoliopage-blocks/AppPortfolioPageGallery.vue
+++ src/components/portfoliopage-blocks/AppPortfolioPageGallery.vue
@@ -4,17 +4,17 @@
       <h2 class="title" v-html="pageData['galleryTitle']"></h2>
       <div class="gallery">
         <AppGalleryEl v-for="(el, index) in iteratedList"
-                      :key="el['galleryListProductName']"
+                      :key="el['ID']"
                       :page="'portfolio-page'"
-                      :elementData="el"
-                      @click="galleryModal(true, index)"
+                      :elementData="el['acf']"
+                      @click="galleryModal(true, index, el['acf']['galleryListProductHash'])"
         />
       </div>
 
       <button class="btn btn-blue"
               v-if="iteratedListLength > shownLimit"
               @click="this.shownLimit+=10">
-        {{pageData['galleryButtonText']}}
+        {{ pageData['galleryButtonText'] }}
       </button>
       <h3 v-else class="text-info">And much <b>more</b>...</h3>
     </div>
@@ -23,7 +23,7 @@
     <AppGalleryModal
         v-if="galleryModalActive"
         @galleryModal="galleryModal"
-        :modalData="iteratedList[currentIndex]"
+        :modalData="iteratedList[currentIndex]['acf']"
         :pageData="pageData"
     />
   </transition>
@@ -36,30 +36,80 @@ import {defineComponent} from "vue";
 
 export default defineComponent({
   name: "AppPortfolioPageGallery",
-  components: {AppGalleryEl, AppGalleryModal},
   props: {
     pageData: Object
   },
+  components: {AppGalleryEl, AppGalleryModal},
   data() {
     return {
       galleryModalActive: false,
       currentIndex: 0,
       shownLimit: 10,
+      modalChecked: false
     }
   },
   computed: {
-    iteratedList(){
-      return this.pageData?.['galleryList'] ? this.pageData['galleryList'].slice(0,this.shownLimit) : this.pageData?.['galleryList']
+    iteratedList() {
+      if (typeof this.pageData === "object") {
+        return this.pageData['galleryList'].slice(0, this.shownLimit)
+      }
+      return []
     },
-    iteratedListLength(){
-      return  this.pageData?.['galleryList'] ? this.pageData['galleryList'].length : 10
+    iteratedListLength() {
+      if (typeof this.pageData === "object") {
+        return this.pageData['galleryList'].length
+      }
+      return 10
+    },
+    routeHash() {
+      return this.$route['hash'];
+    },
+    routePath() {
+      return this.$route['path'];
+    },
+    routeName() {
+      return this.$route['name'];
+    },
+    galleryList() {
+      if (typeof this.pageData === "object") {
+        return this.pageData['galleryList']
+      }
+      return []
     }
   },
   methods: {
-    galleryModal(trigger: boolean, cardIndex: number) {
+    galleryModal(trigger: boolean, cardIndex: number, hashName: string) {
       this.galleryModalActive = trigger;
       this.currentIndex = cardIndex;
       trigger ? document.body.style.overflowY = "hidden" : document.body.style.overflowY = "auto"
+      if (trigger && !window.location.hash && this.routeName !== "home") {
+        location.hash = hashName
+      } else if (!trigger) {
+        history.replaceState(undefined, '', this.routePath)
+      }
+    },
+    galleryModalHash(galleryData: number[]) {
+      if (
+          !this.modalChecked &&
+          this.routeName != "home" &&
+          this.routeHash
+      ) {
+        let index = galleryData.findIndex((elem: object | any) => {
+          return elem['acf']['galleryListProductHash'] === this.routeHash
+        })
+        if (index >= 0) {
+          this.galleryModal(true, index, '')
+          this.modalChecked = true
+        }
+      }
+    }
+  },
+  mounted() {
+    this.galleryModalHash(this.galleryList)
+  },
+  watch: {
+    galleryList(newData) {
+      this.galleryModalHash(newData)
     }
   }
 })
@@ -68,22 +118,26 @@ export default defineComponent({
 <style lang="scss">
 .gallery-block {
   padding: 180px 0 80px;
+
   .title {
     width: 525px;
     font-weight: 300;
     font-size: 30px;
     line-height: 120%;
   }
+
   .gallery {
     display: flex;
     flex-wrap: wrap;
     gap: 60px;
     margin: 80px 0 0;
   }
+
   .btn {
     margin: 60px auto 0;
   }
-  .text-info{
+
+  .text-info {
     text-align: center;
     font-weight: 300;
     font-size: 30px;
@@ -95,17 +149,21 @@ export default defineComponent({
 @media (max-width: 1025px) {
   .gallery-block {
     padding: 140px 0 60px;
+
     .title {
       width: 510px;
       font-size: 26px;
     }
+
     .gallery {
       gap: 30px;
       margin: 40px 0 0;
     }
+
     .btn {
     }
-    .text-info{
+
+    .text-info {
       margin: 40px 0 0;
       font-size: 26px;
     }
@@ -115,23 +173,29 @@ export default defineComponent({
 @media (max-width: 660px) {
   .gallery-block {
     padding: 110px 0 40px;
+
     .title {
       max-width: 100%;
       font-size: 20px;
       text-align: center;
     }
+
     .gallery {
       display: flex;
       flex-direction: column;
     }
+
     .btn {
       margin: 30px auto 0;
+
       .text {
       }
+
       .circled-blue-arrow {
       }
     }
-    .text-info{
+
+    .text-info {
       margin: 30px 0 0;
       font-size: 20px;
     }
