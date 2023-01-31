f3c3bfc3cd88a2e27883793741cc1fcb48c586bc
Anton Vorobiov <34216721+drageryort@users.noreply.github.com>
2023-01-31 02:03:00 +0200

    SEO

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  pluginOptions: {
+    sitemap: {
+      urls: [
+        'https://esthete.studio/',
+        'https://esthete.studio/web',
+        'https://esthete.studio/applications',
+        'https://esthete.studio/design',
+        'https://esthete.studio/portfolio',
+      ]
+    }
+  }
+}
